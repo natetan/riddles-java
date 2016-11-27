85f725348e3665fde155961c21cc796da3befe20
Yulong Tan <yulongtan67@gmail.com>
2016-11-26 23:09:18 -0800

    Riddle manager

---
+++ src/RiddleManager.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by yulong on 11/26/16.
+ */
+
+public class RiddleManager {
+
+    public static final String FILE = "riddles.txt";
+
+    private List<Riddle> riddles;
+
+    public RiddleManager() {
+        try {
+            Scanner file = new Scanner(new File(FILE));
+            int count = 1;
+            while (file.hasNextLine()) {
+                String riddle = file.nextLine();
+                String answer = file.nextLine();
+                riddles.add(new Riddle(riddle, answer, count));
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getRiddle() {
+        int rand = (int) (Math.random() * this.riddles.size());
+        return this.getRiddle(rand);
+    }
+
+    public String getRiddle(int riddleNumber) {
+        if (riddleNumber < 0 || riddleNumber >= this.riddles.size()) {
+            throw new IllegalArgumentException("Number is invalid");
+        }
+        return this.riddles.get(riddleNumber).toString();
+    }
+
+    public int numberOfRiddles() {
+        return this.riddles.size();
+    }
+}
