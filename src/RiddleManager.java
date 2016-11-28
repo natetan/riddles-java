ec743091e224657eb57aadd030709d8195c41d24
Yulong Tan <yulongtan67@gmail.com>
2016-11-28 15:55:59 -0800

    fix nullpointerexception

--- src/RiddleManager.java
+++ src/RiddleManager.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class RiddleManager {
 	private List<Riddle> riddles;
 
 	public RiddleManager() {
+		this.riddles = new ArrayList<>();
 		try {
 			Scanner file = new Scanner(new File(FILE));
 			int count = 1;
