b6d8291d9d34a913f972c3b7f84839e8080505d5
Yulong Tan <yulongtan67@gmail.com>
2016-11-28 15:48:20 -0800

    testing riddles

---
+++ src/RiddleMain.java
@@ -0,0 +1,11 @@
+/**
+ * Created by yulong on 11/28/16.
+ */
+
+public class RiddleMain {
+
+	public static void main(String[] args) {
+		RiddleManager manager = new RiddleManager();
+		System.out.println("Getting riddle: " + manager.getRiddle());
+	}
+}
